65d8d4f0a2b70d60d7b28f955e63a24b7e0be383
sanchezju <sjuan634@yahoo.com>
2023-01-29 09:36:30 -0800

    rearrange seed order

--- seeds/index.js
+++ seeds/index.js
@@ -10,20 +10,20 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
+
+  await seedRatings();
+  console.log('\n----- RATINGS SEEDED -----\n');
+
   await seedUsers();
   console.log('\n----- USERS SEEDED -----\n');
 
   await seedRecipes();
   console.log('\n----- RECIPES SEEDED -----\n');
 
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
-
   await seedReviews();
   console.log('\n----- REVIEWS SEEDED -----\n');
-
-  await seedRatings();
-  console.log('\n----- RATINGS SEEDED -----\n');
 
   process.exit(0);
 };
