0fa712005b4b2379236e69a3340e16bfbf78a2b1
sanchezju <sjuan634@yahoo.com>
2023-01-29 20:19:53 -0800

    add routes to test db interaction on Insomnia

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const recipesRoutes = require('./recipeRoutes');
+const reviewRoutes = require('./reviewRoutes');
 
 router.use('/users', userRoutes);
+router.use('/recipes', recipesRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -2,39 +2,48 @@ const router = require('express').Router();
 const { Recipe } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const newRecipe = await Recipe.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
+    const newRecipe = await Recipe.create(req.body);
     res.status(200).json(newRecipe);
-
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const recipe = await Recipe.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const newRecipe = await Recipe.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
 
-    if (!recipe) {
-      res.status(404).json({ message: 'No recipe found with this id!' });
-      return;
-    }
+//     res.status(200).json(newRecipe);
 
-    res.status(200).json(recipe);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.delete('/:id', withAuth, async (req, res) => {
+//   try {
+//     const recipe = await Recipe.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!recipe) {
+//       res.status(404).json({ message: 'No recipe found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(recipe);
+
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
