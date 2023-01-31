54980eb3b4b95661efcf32a42c7d17f92d5f9819
sanchezju <sjuan634@yahoo.com>
2023-01-30 20:39:30 -0800

    add review and rating routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipesRoutes = require('./recipeRoutes');
 const reviewRoutes = require('./reviewRoutes');
+const ratingRoutes = require('./ratingRoutes');
 const testRoutes = require('./testRoutes');
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipesRoutes);
 router.use('/reviews', reviewRoutes);
-router.use('./test', testRoutes);
+router.use('/test', testRoutes);
 
 module.exports = router;
