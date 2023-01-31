686c12fc9c3daf8215335a49cccf01fed0ec53b4
sanchezju <sjuan634@yahoo.com>
2023-01-30 18:49:21 -0800

    add user_id and recipe_id

--- seeds/rating-seeds.js
+++ seeds/rating-seeds.js
@@ -2,19 +2,29 @@ const { Rating } = require('../models');
 
 const ratingData = [
   {
-    number: 1
+    number: 1,
+    user_id: 1,
+    recipe_id: 2,
   },
   {
-    number: 2
+    number: 2,
+    user_id: 1,
+    recipe_id: 3,
   },
   {
-    number: 3
+    number: 3,
+    user_id: 4,
+    recipe_id: 1,
   },
   {
-    number: 4
+    number: 4,
+    user_id: 2,
+    recipe_id: 2,
   },
   {
-    number: 5
+    number: 5,
+    user_id: 3,
+    recipe_id: 3,
   }
 ];
 
