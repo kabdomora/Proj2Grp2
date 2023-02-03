db522f33183e9736be340202e1038ee718699b38
sanchezju <sjuan634@yahoo.com>
2023-02-02 23:14:40 -0800

    add profile page with delete button

---
+++ public/js/profile.js
@@ -0,0 +1,20 @@
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/recipes/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete recipe');
+    }
+  }
+};
+
+
+document
+  .querySelector('.recipe-list')
+  .addEventListener('click', delButtonHandler);
