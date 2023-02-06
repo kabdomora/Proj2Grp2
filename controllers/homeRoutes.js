89a09fc8543ec9f6eb3a032d8d8e5cfcee476a3b
sanchezju <sjuan634@yahoo.com>
2023-02-05 20:49:06 -0800

    add user model to recipe

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,6 +37,10 @@ router.get('/recipe/:id', async (req, res) => {
         ],
         },
         {
+          model: User,
+          attributes: ['name'],
+        },
+        {
           model: Review,
           attributes: ['review',],
           include: [
@@ -46,10 +50,6 @@ router.get('/recipe/:id', async (req, res) => {
             },
           ],
         },
-        {
-          model: Rating,
-          attributes: ['score'],
-        }
       ],
     });
 
